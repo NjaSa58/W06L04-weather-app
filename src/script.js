de2257033acb63c41ce9984965a1c3b6332494a3
juri006 <58621975+juri006@users.noreply.github.com>
2022-10-11 21:14:17 +0200

    API 5 days forecast integrated

--- src/script.js
+++ src/script.js
@@ -19,7 +19,8 @@ let days = [
   "Saturday",
 ];
 
-function displayForecast() {
+function displayForecast(response) {
+  console.log(response.data.daily);
   let forecastElement = document.querySelector("#forecast");
 
   let days = ["Mon", "Tue", "Wed", "Thu", "Fri"];
@@ -46,10 +47,15 @@ function displayForecast() {
   forecastElement.innerHTML = forecastHTML;
 }
 
-displayForecast();
-
 let currentDay = days[now.getDay()];
 currentDateTime.innerHTML = `${currentDay} ${hours}:${minutes}`;
+
+function getForecast(coordinates) {
+  let apiKey = "a43564c91a6c605aeb564c9ed02e3858";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&unit=metric`;
+  console.log(apiUrl);
+  axios.get(apiUrl).then(displayForecast);
+}
 
 function displayWeatherCondition(response) {
   document.querySelector("#city-name").innerHTML = response.data.name;
@@ -81,11 +87,12 @@ function displayWeatherCondition(response) {
   iconElement.setAttribute("alt", weatherDescription);
 
   celsiusTemperature = response.data.main.temp;
-  console.log(celsiusTemperature);
+
+  getForecast(response.data.coord);
 }
 
 function searchCity(city) {
-  let apiKey = "6a5332815e8c548aa0bb0cba1b36ef91";
+  let apiKey = "a43564c91a6c605aeb564c9ed02e3858";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayWeatherCondition);
 }
@@ -98,7 +105,7 @@ function handleSubmit(event) {
 }
 
 function searchLocation(position) {
-  let apiKey = "6a5332815e8c548aa0bb0cba1b36ef91";
+  let apiKey = "f81614abe2395d5dfecd45b9298041de";
   let lat = position.coords.latitude;
   let lon = position.coords.longitude;
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
