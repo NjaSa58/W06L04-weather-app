5bc88fdc12e0e0ac34a230a2398ac9b30f508f97
juri006 <58621975+juri006@users.noreply.github.com>
2022-10-11 22:02:56 +0200

    5 days API integrated and functional

--- src/script.js
+++ src/script.js
@@ -19,28 +19,41 @@ let days = [
   "Saturday",
 ];
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[day];
+}
+
 function displayForecast(response) {
   console.log(response.data.daily);
+  let forecast = response.data.daily;
   let forecastElement = document.querySelector("#forecast");
-
-  let days = ["Mon", "Tue", "Wed", "Thu", "Fri"];
 
   let forecastHTML = `<div class="row">`;
 
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `<div class="col">
-            ${day} <br /><img src="img/grmljavina.png" alt="day1" width="50px" />
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 5) {
+      forecastHTML =
+        forecastHTML +
+        `<div class="col">
+            ${formatDay(
+              forecastDay.dt
+            )} <br /><img src="https://openweathermap.org/img/wn/${
+          forecastDay.weather[0].icon
+        }.png" alt="day1" width="50px" />
             <br />
             <span class="weather-forecast-temperature-min">
-            19°C
+            ${Math.round(forecastDay.temp.min)}°C
             </span>
             <br />
             <span class="weather-forecast-temperature-max">
-            27°C
+            ${Math.round(forecastDay.temp.max)}°C
             </span>
           </div>`;
+    }
   });
 
   forecastHTML = forecastHTML + `</div>`;
@@ -52,7 +65,7 @@ currentDateTime.innerHTML = `${currentDay} ${hours}:${minutes}`;
 
 function getForecast(coordinates) {
   let apiKey = "a43564c91a6c605aeb564c9ed02e3858";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&unit=metric`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   console.log(apiUrl);
   axios.get(apiUrl).then(displayForecast);
 }
